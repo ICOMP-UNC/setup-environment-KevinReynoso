cee22c62b8d4cb4c1be563156a7b4d19578d0a49
Kevin <kevin.reynoso@mi.unc.edu.ar>
2024-03-21 15:12:25 -0300

    CL:
    - Fix minor typos

---
+++ include/logic.h
@@ -0,0 +1,9 @@
+#pragma once
+
+int add(int first_number, int second_number);
+
+int substract(int first_number, int second_number);
+
+int multiply(int first_number, int second_number);
+
+double divide(int first_number, int second_number);
